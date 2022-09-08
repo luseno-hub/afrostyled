2ddf42962e391816ca474e1ad8096185e3bb95b4
luseno-hub <luseno@outlook.com>
2022-09-08 13:58:18 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const dataSet = [
+    {
+      category: "Sporting Goods",
+      price: "$49.99",
+      stocked: true,
+      name: "Football",
+    },
+    {
+      category: "Sporting Goods",
+      price: "$9.99",
+      stocked: true,
+      name: "Baseball",
+    },
+    {
+      category: "Sporting Goods",
+      price: "$29.99",
+      stocked: false,
+      name: "Basketball",
+    },
+    {
+      category: "Electronics",
+      price: "$99.99",
+      stocked: true,
+      name: "iPod Touch",
+    },
+    {
+      category: "Electronics",
+      price: "$399.99",
+      stocked: false,
+      name: "iPhone 5",
+    },
+    {
+      category: "Electronics",
+      price: "$199.99",
+      stocked: true,
+      name: "Nexus 7",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
